6d992b4739da85f7d09caccb16ddddea32d1ef88
Galilea Lopez <galilea.lopez@michelada.io>
2019-08-06 17:33:15 -0500

    Add test

--- test/controllers/ektar/organizations_controller_test.rb
+++ test/controllers/ektar/organizations_controller_test.rb
@@ -3,6 +3,9 @@ require "test_helper"
 module Ektar
   class OrganizationsControllerTest < ActionDispatch::IntegrationTest
     include Engine.routes.url_helpers
+    def setup
+      @organization = ektar_organizations(:main_organization)
+    end
 
     test 'should get index' do
       get organizations_path
@@ -17,36 +20,57 @@ module Ektar
     end
 
     test 'should get show' do
-      get organization_path
+      get organization_path(@organization.id)
 
       assert_response :success
     end
 
     test 'should get edit' do
-      get edit_organization_path
+      get edit_organization_path(@organization.id)
 
       assert_response :success
     end
 
-    test 'can create organization' do
+    test 'can create organization' do 
+      assert_difference 'Ektar::Organization.count', 1 do
+        post organizations_path(valid_organization)
+      end
+
+      assert_equal  'Organization test', Ektar::Organization.last.name
     end
 
     test 'can show organization' do
+      get organization_path(@organization.id)
+
+      assert_select 'form'
     end
     
-    test 'can edit organization' do
+    test 'can update organization' do
+      put organization_path(@organization.id), params: {organization: {name: "michelada"}}
+      @organization.reload
+      
+      assert_equal "michelada", @organization.name
     end
 
     test 'can delete organization' do
+      organization_delete = ektar_organizations(:organization_delete)
+
+      assert_difference 'Ektar::Organization.count', -1 do
+        delete organization_path(organization_delete.id)
+      end
     end
 
-    test 'can not show invalid organization' do
-    end
+    # test 'can not show invalid organization' do
+    # end
 
-    test 'can not edit invalid organization' do
-    end
+    # test 'can not edit invalid organization' do
+    # end
 
-    test 'can not delete invalid organization' do
+    # test 'can not delete invalid organization' do
+    # end
+
+    def valid_organization
+      {organization: { name: 'Organization test', enable: true}}
     end
   end
 end
