cf8fd1e73f28e548e5f24b90fa70737471b836c3
Galilea Lopez <galilea.lopez@michelada.io>
2019-08-07 17:55:13 -0500

    Add test user controller

--- test/controllers/ektar/user_controller_test.rb
+++ test/controllers/ektar/user_controller_test.rb
@@ -4,8 +4,32 @@ module Ektar
   class UserControllerTest < ActionDispatch::IntegrationTest
     include Engine.routes.url_helpers
 
-    # test "the truth" do
-    #   assert true
-    # end
+    test "should get index" do
+    end
+
+    test "should get new" do
+    end
+
+    test "should get show" do
+    end
+
+    test "should get edit" do
+    end
+
+    test "can create user" do
+    end
+
+    test "can edit user" do
+    end
+
+    test "can delete user" do
+    end
+
+    def valid_user
+      organization = ektar_organizations(:main_organization)
+      {email: "mario@gmail.com",
+       encrypted_password: "12345678",
+       ektar_organization_id: organization.id,}
+    end
   end
 end
