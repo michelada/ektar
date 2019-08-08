2bd67b6a8e10d1e64e0b57bba76988599d474455
Galilea Lopez <galilea.lopez@michelada.io>
2019-08-08 11:17:51 -0500

    Add user test

--- test/controllers/ektar/user_controller_test.rb
+++ test/controllers/ektar/user_controller_test.rb
@@ -3,17 +3,36 @@ require "test_helper"
 module Ektar
   class UserControllerTest < ActionDispatch::IntegrationTest
     include Engine.routes.url_helpers
+    def setup
+      @user = ektar_users(:first_user)
+    end
 
     test "should get index" do
+      get users_path
+
+      assert_response :success
+      assert_select "th", text: "Email"
     end
 
     test "should get new" do
+      get new_user_path
+
+      assert_response :success
+      assert_select ".input"
     end
 
     test "should get show" do
+      get users_path(@user.id)
+
+      assert_response :success
     end
 
     test "should get edit" do
+      get edit_user_path(@user.id)
+
+      assert_response :success
+      assert_select ".input", value: @user.email
+      assert_select ".input"
     end
 
     test "can create user" do
