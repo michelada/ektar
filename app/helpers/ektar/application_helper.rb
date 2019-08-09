7ef8699f8ef26e1bf59750f00b9867b978a1d564
Mildred Nataly <nataly.silva@michelada.io>
2019-08-09 11:05:30 -0500

    Modify method on application helper

--- app/helpers/ektar/application_helper.rb
+++ app/helpers/ektar/application_helper.rb
@@ -13,19 +13,11 @@ module Ektar
       attributes_options.each do |attribute|
         if attribute.keys[0] == attribute_name
           attribute.values[0].split(',') do |option|
-            if clean_data(option).nil?
-              options += [t('forms.dropdown_options', option: option)]
-            else
-              options += [clean_data(option)]
-            end
+            options += [t('forms.dropdown_options', option: option)]
           end
         end
       end
       options
-    end
-
-    def clean_data(option)
-      option.to_i if option.match(/\A[+-]?\d+?(\.\d+)?\Z/)
     end
   end
 end
