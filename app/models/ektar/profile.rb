0cb3a5ea2167294aad61c05eae8959a69129e3eb
Mildred Nataly <nataly.silva@michelada.io>
2019-08-09 17:50:45 -0500

    modify fixtures

--- app/models/ektar/profile.rb
+++ app/models/ektar/profile.rb
@@ -1,6 +1,6 @@
 module Ektar
   class Profile < ApplicationRecord
-    belongs_to :ektar_user, class_name: 'Ektar::User', foreign_key: :ektar_user_id
+    belongs_to :user, class_name: 'Ektar::User', foreign_key: :ektar_user_id
     has_one_attached :image_profile
     validates :name, presence: true
   end
