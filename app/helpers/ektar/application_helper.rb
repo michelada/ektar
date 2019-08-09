a272c9128f8a608963fe09c1d2928cdd2d9b73b1
Mildred Nataly <nataly.silva@michelada.io>
2019-08-08 23:18:52 -0500

    Fix select Save organization within a user Method on application helper to send values

--- app/helpers/ektar/application_helper.rb
+++ app/helpers/ektar/application_helper.rb
@@ -7,5 +7,25 @@ module Ektar
     def current_webpacker_instance
       Ektar.webpacker
     end
+
+    def select_options(attribute_name)
+      options = []
+      attributes_options.each do |attribute|
+        if attribute.keys[0] == attribute_name
+          attribute.values[0].split(',') do |option|
+            if clean_data(option).nil?
+              options += [t('forms.dropdown_options', option: option)]
+            else
+              options += [clean_data(option)]
+            end
+          end
+        end
+      end
+      options
+    end
+
+    def clean_data(option)
+      option.to_i if option.match(/\A[+-]?\d+?(\.\d+)?\Z/)
+    end
   end
 end
