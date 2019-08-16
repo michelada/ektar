18c2599812a44d2f924d12b540c39c4d86550b08
Mario Alberto Chávez <mario.chavez@gmail.com>
2019-08-16 12:53:21 -0500

    Refactor organizations

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,6 @@
 Ektar::Engine.routes.draw do
   root to: "organizations#index"
 
-  namespace :admin do
-    resources :organizations do
-      resources :users
-    end
-  end
-
   resources :organizations do
     resources :users do
       resource :profiles
