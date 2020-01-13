583038080e501e309dd82512276f0be0d9d4ff9e
Mario Alberto Chávez <mario.chavez@gmail.com>
2020-01-13 17:40:25 -0600

    Adds assets linters

--- app/javascript/js/flash.js
+++ app/javascript/js/flash.js
@@ -1,22 +1,21 @@
-export class Flash {
+export default class Flash {
   static clean() {
-    const flash = document.querySelector('.message-container')
+    const flash = document.querySelector(".message-container");
     if (flash) {
       flash.remove();
     }
   }
 
   static setup() {
-    const close = document.querySelector('button.delete');
+    const close = document.querySelector("button.delete");
     if (close) {
-
       const autoClose = setTimeout(() => {
         close.click();
         clearTimeout(autoClose);
       }, 2000);
 
-      close.addEventListener('click', () => {
-        const flash = document.querySelector('.message-container')
+      close.addEventListener("click", () => {
+        const flash = document.querySelector(".message-container");
         if (flash) {
           flash.remove();
         }
