33c0a5c2484ab7e428d667fd6c4a52cb2bd13f7c
Gerald Amezcua <gerald.amezcua@michelada.io>
2020-01-16 11:04:14 -0600

    Add Plan tests

--- test/models/ektar/plan_test.rb
+++ test/models/ektar/plan_test.rb
@@ -2,8 +2,13 @@ require "test_helper"
 
 module Ektar
   class PlanTest < ActiveSupport::TestCase
-    # test "the truth" do
-    #   assert true
-    # end
+    test "plan has default values" do
+      subject = ektar_plans(:plan)
+
+      refute subject.free?
+      assert_equal subject.trial, 0
+      assert_equal subject.price, 0
+      assert subject.active?
+    end
   end
 end
