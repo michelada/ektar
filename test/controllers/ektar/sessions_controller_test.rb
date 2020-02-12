125ae241252d675ee5c9c7b21da7cf5cdac29c9a
Juan Brizuela <juan.brizuela@michelada.io>
2020-02-12 16:34:59 -0600

    Added sessions controller tests

--- test/controllers/ektar/sessions_controller_test.rb
+++ test/controllers/ektar/sessions_controller_test.rb
@@ -3,9 +3,25 @@ require "test_helper"
 module Ektar
   class SessionsControllerTest < ActionDispatch::IntegrationTest
     include Engine.routes.url_helpers
+    include LoginHelper
 
-    # test "the truth" do
-    #   assert true
-    # end
+    test "normal user is redirected_to [ND]" do
+      skip # ND
+    end
+
+    test "admin user is redirected_to users#index" do
+      sign_in ektar_users(:admin_user)
+      get root_path
+
+      assert_redirected_to users_path
+    end
+
+    test "super_admin user is redirected to organizations#index" do
+      sign_in ektar_users(:super_user)
+      get root_path
+
+      assert_equal "index", @controller.action_name
+      assert_equal "organizations", @controller.controller_name
+    end
   end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -6,6 +6,7 @@ require_relative "../test/dummy/config/environment"
 ActiveRecord::Migrator.migrations_paths = [File.expand_path("../test/dummy/db/migrate", __dir__)]
 ActiveRecord::Migrator.migrations_paths << File.expand_path("../db/migrate", __dir__)
 require "rails/test_help"
+require "support/login_helper"
 
 # Filter out the backtrace from minitest while preserving the one from other libraries.
 Minitest.backtrace_filter = Minitest::BacktraceFilter.new
