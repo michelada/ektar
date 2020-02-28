b2ef9a4f8de0b554954619bf8d97839f238c61a1
Gerald Amezcua <gerald.amezcua@michelada.io>
2020-02-28 22:05:46 +0000

    [FIX] not found routes [ADD] ektar_url_helpers as a shorthand for the helpers module

--- app/controllers/ektar/super_admin/base_controller.rb
+++ app/controllers/ektar/super_admin/base_controller.rb
@@ -7,24 +7,24 @@ module Ektar
     sig { returns(String) }
     def new_resource_path
       path = "new_super_admin_#{resource_class.model_name.singular_route_key}_path"
-      send(path)
+      ektar_url_helpers.send(path)
     end
 
     sig { params(resource: ActiveRecord::Base).returns(String) }
     def edit_resource_path(resource)
       path = "edit_super_admin_#{resource.model_name.singular_route_key}_path"
-      send(path, resource)
+      ektar_url_helpers.send(path, resource)
     end
 
     sig { params(resource: ActiveRecord::Base).returns(String) }
     def resource_path(resource)
       path = "super_admin_#{resource.model_name.singular_route_key}_path"
-      send(path, resource)
+      ektar_url_helpers.send(path, resource)
     end
 
     sig { returns(String) }
     def collection_path
-      send "super_admin_#{resource_class.model_name.route_key}_path"
+      ektar_url_helpers.send("super_admin_#{resource_class.model_name.route_key}_path")
     end
   end
 end
